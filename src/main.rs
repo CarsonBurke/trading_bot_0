2c7b2b54617d812e8ba7f39254de8b0a1b0904a3
Carson <carsonburke22@gmail.com>
2024-11-25 17:00:47 -0800

    basic connection and account info getting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use ibapi::{accounts::{AccountSummaries, AccountSummaryTags}, client::Subscription, Client};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Trying to connect to IB API!");
+    let connection_url = "127.0.0.1:4001";
+
+    let client = Client::connect(connection_url, 1).expect("connection to TWS failed!");
+    println!("Successfully connected to TWS at {connection_url}");
+
+    account_info(&client);
+}
+
+fn account_info(client: &Client) {
+    let subscription = client.account_summary("All", AccountSummaryTags::ALL).expect("error requesting account summary");
+
+    for update in &subscription {
+        match update {
+            AccountSummaries::Summary(summary) => println!("{summary:?}"),
+            AccountSummaries::End => subscription.cancel(),
+        }
+    }
 }
