6a0ee60dd55361e6a197e2bb0d07dc93a37ddcf1
Carson <carsonburke22@gmail.com>
2024-11-25 17:38:20 -0800

    getting daily historical data for tesla

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use ibapi::{accounts::{AccountSummaries, AccountSummaryTags}, client::Subscription, Client};
+use clap::{arg, Command};
+use ibapi::{
+    accounts::{AccountSummaries, AccountSummaryTags},
+    client::Subscription,
+    contracts::Contract,
+    market_data::historical::{BarSize, ToDuration, WhatToShow},
+    Client,
+};
+use time::macros::datetime;
 
 fn main() {
     println!("Trying to connect to IB API!");
@@ -8,10 +16,13 @@ fn main() {
     println!("Successfully connected to TWS at {connection_url}");
 
     account_info(&client);
+    historical_data(&client);
 }
 
 fn account_info(client: &Client) {
-    let subscription = client.account_summary("All", AccountSummaryTags::ALL).expect("error requesting account summary");
+    let subscription = client
+        .account_summary("All", AccountSummaryTags::ALL)
+        .expect("error requesting account summary");
 
     for update in &subscription {
         match update {
@@ -20,3 +31,31 @@ fn account_info(client: &Client) {
         }
     }
 }
+
+fn historical_data(client: &Client) {
+    let ticker = "TSLA";
+
+    let contract = Contract::stock(ticker);
+
+    let historical_data = client
+        .historical_data(
+            &contract,
+            datetime!(2023-04-11 20:00 UTC),
+            1.days(),
+            BarSize::Hour,
+            WhatToShow::Trades,
+            true,
+        )
+        .expect("historical data request failed");
+
+    println!(
+        "start: {:?}, end: {:?}",
+        historical_data.start, historical_data.end
+    );
+
+    for bar in &historical_data.bars {
+        println!("{bar:?}");
+    }
+}
+
+fn find_opportunites(client: &Client) {}
