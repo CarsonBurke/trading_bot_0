22182465d3abf7450a86ee9c17e72e8279ff5ab4
CarsonBurke <carsonburke22@gmail.com>
2026-04-27 10:06:16 -0700

    refactor(rmsnorm): drop learnable weight gain

--- trading_bots/src/torch/model/rmsnorm.rs
+++ trading_bots/src/torch/model/rmsnorm.rs
@@ -1,38 +1,23 @@
-use tch::nn::{self, Init, Linear};
+use tch::nn::{self, Linear};
 use tch::Tensor;
 
 pub(super) struct RMSNorm {
-    weight: Tensor,
+    dim: i64,
     eps: f64,
 }
 
 impl RMSNorm {
-    pub(super) fn new(p: &nn::Path, dim: i64, eps: f64) -> Self {
-        let weight = p.var("weight", &[dim], Init::Const(1.0));
-        Self { weight, eps }
+    pub(super) fn new(_p: &nn::Path, dim: i64, eps: f64) -> Self {
+        Self { dim, eps }
     }
 
     pub(super) fn forward(&self, x: &Tensor) -> Tensor {
-        let normalized_shape = [self.weight.size()[0]];
-        let weight = if self.weight.kind() == x.kind() {
-            self.weight.shallow_clone()
-        } else {
-            self.weight.to_kind(x.kind())
-        };
-        x.internal_fused_rms_norm(normalized_shape, Some(&weight), Some(self.eps))
+        x.internal_fused_rms_norm([self.dim], None::<&Tensor>, Some(self.eps))
             .0
     }
 
     pub(super) fn forward_linear(&self, x: &Tensor, linear: &Linear) -> Tensor {
         let x = self.forward(x);
         super::linear_with_same_dtype(&x, linear)
-    }
-
-    pub(super) fn weight(&self) -> &Tensor {
-        &self.weight
-    }
-
-    pub(super) fn eps(&self) -> f64 {
-        self.eps
     }
 }
